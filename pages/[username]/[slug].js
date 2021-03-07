bf22ddefd7127a4106760a8e99088a1d8bf83bdb
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-03-06 19:02:11 -0700

    Firebase emulator setup and routes

---
+++ pages/[username]/[slug].js
@@ -0,0 +1,7 @@
+export default function UserSlugPage({}) {
+  return (
+    <main>
+      <h1>Slug Page</h1>
+    </main>
+  );
+}
---
+++ pages/admin/[slug].js
@@ -0,0 +1,7 @@
+export default function AdminSlugPage({}) {
+  return (
+    <main>
+      <h1>Slug Page</h1>
+    </main>
+  );
+}
