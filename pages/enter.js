65f2610d2502c133bb632de7bc8f5fddfe6f66f1
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-03-06 22:08:36 -0700

    Authentication with google

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,48 @@
-export default function EnterPage({}) {
+import { auth, googleAuthProvider } from "../lib/firebase";
+
+import toast from "react-hot-toast";
+
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
+
+export default function Enter(props) {
+  const { user, username } = useContext(UserContext);
+
   return (
     <main>
-      <h1>Sign In</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
 }
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    try {
+      await auth.signInWithPopup(googleAuthProvider);
+    } catch (err) {
+      console.log(err);
+      toast.error("Signin Failed");
+    }
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} />
+      Sign In With Google
+    </button>
+  );
+}
+
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {}
