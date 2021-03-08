3f29b06634ac03b231829db448545c432503c507
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-03-07 20:04:17 -0700

    Auth with custom usernames

--- pages/enter.js
+++ pages/enter.js
@@ -1,8 +1,9 @@
 import { auth, googleAuthProvider } from "../lib/firebase";
 
 import toast from "react-hot-toast";
+import debounce from "lodash.debounce";
 
-import { useContext } from "react";
+import { useEffect, useState, useCallback, useContext } from "react";
 import { UserContext } from "../lib/context";
 
 export default function Enter(props) {
@@ -27,6 +28,7 @@ function SignInButton() {
   const signInWithGoogle = async () => {
     try {
       await auth.signInWithPopup(googleAuthProvider);
+      toast.success("Signin Successful");
     } catch (err) {
       console.log(err);
       toast.error("Signin Failed");
@@ -45,4 +47,114 @@ function SignOutButton() {
   return <button onClick={() => auth.signOut()}>Sign Out</button>;
 }
 
-function UsernameForm() {}
+function UsernameForm() {
+  const [formValue, setFormValue] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { user, username } = useContext(UserContext);
+
+  useEffect(() => {
+    checkUsername(formValue);
+  }, [formValue]);
+
+  const onChange = (e) => {
+    const val = e.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+    // usernames cannot be less then 3 chars
+    if (val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (re.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  };
+
+  /**
+   * We use a usernames collection to hold the custom usernames and a reference
+   * to the user document to make the username check faster
+   */
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // create ref for both documents
+      const userDoc = firestore.doc(`users/${user.uid}`);
+      const usernameDoc = firestore.doc(`usernames/${formValue}`);
+
+      const batch = firestore.batch();
+      batch.set(userDoc, {
+        username: formValue,
+        photoURL: user.photoURL,
+        displayName: user.displayName,
+      });
+      batch.set(usernameDoc, { uid: user.uid });
+
+      await batch.commit();
+      toast.success("Added to db succesfully");
+    } catch (err) {
+      console.log(err);
+      toast.error("Failed to add to db");
+    }
+  };
+
+  // This will check the usernams collection in the db to check if the username
+  // is already in use, if it is then it will return false
+  // useCallback allows memoization (save and cache results)
+  // debounce will wait half a second before calling the function
+  const checkUsername = useCallback(
+    debounce(async (username) => {
+      if (username.length >= 3) {
+        // attempt to get ref to username
+        const ref = firestore.doc(`usernames/${username}`);
+        const { exists } = await ref.get();
+        console.log("Firestore read executed");
+        // if ref is not found the username does not yet exist and is valid
+        setIsValid(!exists);
+        setLoading(false);
+      }
+    }, 500),
+    []
+  );
+
+  return (
+    !username && (
+      <section>
+        <h3>Choose Username</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            name="username"
+            placeholder="username"
+            value={formValue}
+            onChange={onChange}
+          />
+          <UsernameMessage
+            username={formValue}
+            isValid={isValid}
+            loading={loading}
+          />
+          <button type="submit" className="btn-green" disabled={!isValid}>
+            Choose
+          </button>
+        </form>
+      </section>
+    )
+  );
+}
+
+function UsernameMessage({ username, isValid, loading }) {
+  if (loading) {
+    return <p>Checking...</p>;
+  } else if (isValid) {
+    return <p className="text-success">{username} is available!</p>;
+  } else if (username && !isValid) {
+    return <p className="text-danger">That username is taken!</p>;
+  } else {
+    return <p></p>;
+  }
+}
