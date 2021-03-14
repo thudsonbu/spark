83bdecb00095bc8e168e5bc7d840af82fe1e3602
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-03-13 21:37:35 -0700

    image upload to storage bucket working

---
+++ components/ImageUploader.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { auth, storage, STATE_CHANGED } from "../lib/firebase";
+import Loader from "./Loader";
+
+// Uploads images to Firebase Storage
+export default function ImageUploader() {
+  const [uploading, setUploading] = useState(false);
+  const [progress, setProgress] = useState(0); // progress of upload as a percentage
+  const [downloadURL, setDownloadURL] = useState(null);
+
+  // Creates a Firebase Upload Task
+  const uploadFile = async (e) => {
+    // Get the file
+    const file = Array.from(e.target.files)[0];
+    const extension = file.type.split("/")[1];
+
+    // Makes reference to the storage bucket location, in the storage bucket we
+    // create a directory for each user that is their uid so it is easy to
+    // locate the file
+    const ref = storage.ref(
+      `uploads/${auth.currentUser.uid}/${Date.now()}.${extension}`
+    );
+    setUploading(true);
+
+    // Starts the upload
+    const task = ref.put(file);
+
+    // Listen to updates to upload task
+    task.on(STATE_CHANGED, (snapshot) => {
+      // calculate percentage of file uploaded
+      const pct = (
+        (snapshot.bytesTransferred / snapshot.totalBytes) *
+        100
+      ).toFixed(0);
+      setProgress(pct);
+    });
+
+    // Get downloadURL AFTER task resolves (Note: this is not a native Promise)
+    task
+      .then((d) => ref.getDownloadURL())
+      .then((url) => {
+        setDownloadURL(url);
+        setUploading(false);
+      });
+  };
+
+  // in the below example we wrap the file input tag in a lable which makes the
+  // upload button much easier to style
+  return (
+    <div className="box">
+      <Loader show={uploading} />
+      {uploading && <h3>{progress}%</h3>}
+
+      {!uploading && (
+        <>
+          <label className="btn">
+            📸 Upload Img
+            <input // the input here will only accept png, gif, or jpeg
+              type="file"
+              onChange={uploadFile}
+              accept="image/x-png,image/gif,image/jpeg"
+            />
+          </label>
+        </>
+      )}
+      {downloadURL && (
+        <code className="upload-snippet">{`![alt](${downloadURL})`}</code>
+      )}
+    </div>
+  );
+}
