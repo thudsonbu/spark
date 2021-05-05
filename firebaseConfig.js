f1a17f3c6bc398f1f330f9b5e1c6c347d0a5f292
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-05-04 21:06:15 -0600

    refector .env vars

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+  authDomain: "spark-c03f1.firebaseapp.com",
+  databaseURL: "https://spark-c03f1-default-rtdb.firebaseio.com",
+  projectId: "spark-c03f1",
+  storageBucket: "spark-c03f1.appspot.com",
+  messagingSenderId: "818433972700",
+  appId: "1:818433972700:web:c3b7eafc8cd45bc8e85719",
+  measurementId: "G-QS2CE12LXN",
+};
