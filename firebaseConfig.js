452638fa6bab728f1430ae2a646e7cdd1289afc4
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-08-30 21:15:23 -0600

    update firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export default {
+const firebaseConfig = {
+  apiKey: "AIzaSyAYzBpc5taIrbRhv2MkaWLraM0RnOIMFvs",
   authDomain: "spark-c03f1.firebaseapp.com",
   databaseURL: "https://spark-c03f1-default-rtdb.firebaseio.com",
   projectId: "spark-c03f1",
   storageBucket: "spark-c03f1.appspot.com",
   messagingSenderId: "818433972700",
   appId: "1:818433972700:web:c3b7eafc8cd45bc8e85719",
-  measurementId: "G-QS2CE12LXN",
+  measurementId: "G-QS2CE12LXN"
 };
+
+export default firebaseConfig;
